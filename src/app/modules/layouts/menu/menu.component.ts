195ed69fa330027b912aa35b1b878deb91e3401b
Amancayjesus136 <amancayjesus136@gmail.com>
2024-10-17 18:37:05 -0500

    img add

--- src/app/modules/layouts/menu/menu.component.ts
+++ src/app/modules/layouts/menu/menu.component.ts
@@ -14,40 +14,8 @@ export class MenuComponent implements OnInit {
     if (isPlatformBrowser(this.platformId)) {
       import('flowbite').then((module) => {
         module.initFlowbite();
-        this.initThemeToggle();
       });
     }
   }
 
-  initThemeToggle() {
-    if (localStorage.getItem('color-theme') === 'light') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-
-    const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      themeToggleLightIcon?.classList.remove('hidden');
-    } else {
-      themeToggleDarkIcon?.classList.remove('hidden');
-    }
-
-    const themeToggleBtn = document.getElementById('theme-toggle');
-
-    themeToggleBtn?.addEventListener('click', () => {
-      themeToggleDarkIcon?.classList.toggle('hidden');
-      themeToggleLightIcon?.classList.toggle('hidden');
-
-      if (localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('color-theme', 'dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
-      }
-    });
-  }
 }
